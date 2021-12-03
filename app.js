5bb5685b692215a13ab4c8b4af8b8cde46a347d1
Anbarasan <anbarasanj24@gmail.com>
2021-12-03 07:43:20 +0530

    Added Query Selctor

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Testing")
+const titles = document.querySelectorAll('title');
+
+const odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+const even = document.querySelectorAll('.list-group-item:nth-child(even)');
+
+for (let i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = '#ccc'
+    even[i].style.backgroundColor = 'royalblue'
+}
